b6a209045ffc3e10a4e9a58aed9c7896541669f7
Saravanan <a@a.com>
2022-11-08 09:55:26 +0530

    Working copy - First page loads after code movement. Further fixes are required.

--- src/components/pokemon-list-page.js
+++ src/components/pokemon-list-page.js
@@ -2,97 +2,24 @@ import { useSelector } from "react-redux";
 
 import PokemonListPagination from "./pokemon-list-pagination";
 import SpinnerComponent from "./spinner-component";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Figure, OverlayTrigger, Popover } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import * as CommonConstants from "../common/commonConstants";
 
 //Display pokemon as list inside pagination
 function PokemonListPage() {
   const pokemonListData = useSelector((state) => state);
+
   return (
-    <div className="App">
-      <h1>Pokémon API</h1>
-      {pokemonListData.action.payload ? (
-        PaginatePokemonFigureList(pokemonListData)
+    <>
+      {pokemonListData.action.payload !== undefined ? (
+        <PokemonListPagination
+          pokeFigureData={pokemonListData}
+        ></PokemonListPagination>
       ) : (
         <h1>
           <SpinnerComponent text="Fetching Pokemon data....." />
         </h1>
       )}
-    </div>
+    </>
   );
-}
-
-//Invokes component and displays Pokemon Figure list with Pagination
-function PaginatePokemonFigureList(pokemonListData) {
-  const pokemonFigureListData = iteratePokemonData(pokemonListData);
-  //console.log(pokemonFigureListData);
-  return (
-    <PokemonListPagination
-      pokeFigureData={pokemonFigureListData}
-    ></PokemonListPagination>
-  );
-}
-
-//Returns Pokemon data as a Figure HTML
-function iteratePokemonData(pokemonListData) {
-  return pokemonListData.action.payload.map((pokeData, id) => {
-    const pokeBio = pokemonDetailsOverlay({ pokedata: pokeData, id: id });
-
-    return pokeBio;
-  });
-}
-
-//Returns overlay html for each pokemon.
-function pokemonDetailsOverlay(pokemonData) {
-  const triggerDispatch = (event) => {
-    window.sessionStorage.setItem("pokeId", +event + 1);
-  };
-
-  const overlayCardData = (
-    <Popover id="popover-basic">
-      <Popover.Header as="h3">
-        {CommonConstants.capitalizeFirstLetter(pokemonData.pokedata.name)}
-      </Popover.Header>
-    </Popover>
-  );
-
-  const pokeBioOVerlay = (
-    <OverlayTrigger
-      key={"overlay-" + pokemonData.id}
-      placement="right"
-      /* delay={{
-        show: CommonConstants.OVERLAY_SHOW_DELAY,
-        hide: CommonConstants.OVERLAY_HIDE_DELAY,
-      }} */
-      overlay={overlayCardData}
-    >
-      <Link
-        key={"link-" + pokemonData.id}
-        to="/pokemonDetails/"
-        onClick={() => triggerDispatch(pokemonData.id)}
-      >
-        <Figure key={"figure-" + pokemonData.id}>
-          <Figure.Image
-            width={CommonConstants.POKE_IMG_SIZE}
-            height={CommonConstants.POKE_IMG_SIZE}
-            alt={pokemonData.pokedata.name}
-            src={
-              CommonConstants.POKE_IMG_URL +
-              (+pokemonData.id + 1) +
-              CommonConstants.POKE_IMG_EXT
-            }
-          />
-          <Figure.Caption>
-            {CommonConstants.capitalizeFirstLetter(pokemonData.pokedata.name)}
-          </Figure.Caption>
-        </Figure>
-      </Link>
-    </OverlayTrigger>
-  );
-  return pokeBioOVerlay;
 }
 
 export default PokemonListPage;
--- src/components/pokemon-list-pagination.js
+++ src/components/pokemon-list-pagination.js
@@ -1,7 +1,8 @@
 import React from "react";
 import * as CommonConstants from "../common/commonConstants";
 import { Pagination } from "react-bootstrap";
-
+import { Figure, OverlayTrigger, Popover } from "react-bootstrap";
+import { Link } from "react-router-dom";
 class PokemonListPagination extends React.Component {
   constructor(props) {
     super(props);
@@ -11,10 +12,87 @@ class PokemonListPagination extends React.Component {
     };
   }
 
+  //Invokes component and displays Pokemon Figure list with Pagination
+  PaginatePokemonFigureList(pokemonListData) {
+    const pokemonFigureListData = this.iteratePokemonData(pokemonListData);
+
+    return (
+      //RENDER METHOD  of pokemon list pagination
+      <div>
+        <Pagination>
+          {this.formPagintionItems(pokemonFigureListData)}
+        </Pagination>
+        {this.state.pageData}
+      </div>
+    );
+  }
+
+  //Returns Pokemon data as a Figure HTML
+  iteratePokemonData(pokemonListData) {
+    return pokemonListData.action.payload.map((pokeData, id) => {
+      const pokeBio = this.pokemonDetailsOverlay({
+        pokedata: pokeData,
+        id: id,
+      });
+
+      return pokeBio;
+    });
+  }
+
+  //Returns overlay html for each pokemon.
+  pokemonDetailsOverlay(pokemonData) {
+    const triggerDispatch = (event) => {
+      window.sessionStorage.setItem("pokeId", +event + 1);
+    };
+
+    const overlayCardData = (
+      <Popover id="popover-basic">
+        <Popover.Header as="h3">
+          {CommonConstants.capitalizeFirstLetter(pokemonData.pokedata.name)}
+        </Popover.Header>
+      </Popover>
+    );
+
+    const pokeBioOVerlay = (
+      <OverlayTrigger
+        key={"overlay-" + pokemonData.id}
+        placement="right"
+        /* delay={{
+        show: CommonConstants.OVERLAY_SHOW_DELAY,
+        hide: CommonConstants.OVERLAY_HIDE_DELAY,
+      }} */
+        overlay={overlayCardData}
+      >
+        <Link
+          key={"link-" + pokemonData.id}
+          to="/pokemonDetails/"
+          onClick={() => triggerDispatch(pokemonData.id)}
+        >
+          <Figure key={"figure-" + pokemonData.id}>
+            <Figure.Image
+              width={CommonConstants.POKE_IMG_SIZE}
+              height={CommonConstants.POKE_IMG_SIZE}
+              alt={pokemonData.pokedata.name}
+              src={
+                CommonConstants.POKE_IMG_URL +
+                (+pokemonData.id + 1) +
+                CommonConstants.POKE_IMG_EXT
+              }
+            />
+            <Figure.Caption>
+              {CommonConstants.capitalizeFirstLetter(pokemonData.pokedata.name)}
+            </Figure.Caption>
+          </Figure>
+        </Link>
+      </OverlayTrigger>
+    );
+    return pokeBioOVerlay;
+  }
+
   componentDidMount() {
     this.setState({
       pageData: this.filterDataForPagination(
-        this.props.pokeFigureData,
+        this.iteratePokemonData(this.props.pokeFigureData),
         this.state.activePageNum
       ),
     });
@@ -23,7 +101,7 @@ class PokemonListPagination extends React.Component {
   //Pagination items onclick change.
   handlePageChange = (pageNum) => {
     let newPageData = this.filterDataForPagination(
-      this.props.pokeFigureData,
+      this.props.pokeFigureData.action.payload,
       pageNum
     );
     this.setState({
@@ -46,7 +124,7 @@ class PokemonListPagination extends React.Component {
         CommonConstants.MAX_ITEMS_PER_PAGE +
       CommonConstants.MAX_ITEMS_PER_PAGE +
       1;
-
+    console.log("pokeFigData", pokeFigData);
     let paginatedItems = pokeFigData.slice(sliceStartIndex, sliceEndIndex - 1);
     //console.log(paginatedItems);
     return paginatedItems;
@@ -77,12 +155,11 @@ class PokemonListPagination extends React.Component {
   };
 
   render() {
+    const allPokeData = this.props.pokeFigureData;
     return (
-      <div>
-        <Pagination>
-          {this.formPagintionItems(this.props.pokeFigureData)}
-        </Pagination>
-        {this.state.pageData}
+      <div className="App">
+        <h1>Pokémon API</h1>
+        {this.PaginatePokemonFigureList(allPokeData)}
       </div>
     );
   }
